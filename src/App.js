8fa6bce83db47bc0bc1d505be41d2650941d9502
Kelly Pham <kelly.pham012@gmail.com>
2020-10-30 12:35:17 -0700

    Fixed modules error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from "./aws-exports";
-import {AmplifySiggOut, withAuthenticator} from "@aws-amplify/ui-react";
+import {AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react";
 
 Amplify.configure(awsconfig)
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AmplifySiggOut />
+        <AmplifySignOut />
         <h1>Content here</h1>
         
       </header>
