1ad5562aac116fab3977e3bb5c3c04da4288c6f3
Kelly Pham <kelly.pham012@gmail.com>
2020-11-02 23:48:53 -0800

    Fixed credential issues

--- src/ProcessImage.js
+++ src/ProcessImage.js
@@ -1,85 +1,61 @@
-import AWS from "aws-sdk";
-require('dotenv').config();
-const AWS_Config = {
-    accessKeyId: process.env.AWS_AccessKey,
-    accessSecretKey: process.env.AWS_AWS_SecretAccessKey,
-    region: "us-east-1"
-}
 
-AWS.config.update(AWS_Config);
-function DetectFaces(imageData) {
+let AWS = require('aws-sdk');
+export default function ProcessImage() {
+    AnonLog();
+    var control = document.getElementById("fileToUpload");
+    var file = control.files[0];
 
-  // Create AWS Rekognition isntance
-  let rekognition = new AWS.Rekognition();
-  let params = {
-    Image: {
-      Bytes: imageData,
-    },
-    Attributes: ["ALL"],
-  };
-  rekognition.detectFaces(params, function (err, data) {
-    if (err) {
-      console.log(err, err.stack);
-    } else {
+    // Load base64 encoded image for display 
+    let reader = new FileReader();
+    reader.onload = (function (theFile) {
+      return function (e) {
+          console.log(e);
+          console.log(e.target.result);
+        //Call Rekognition  
+        AWS.region = "us-east-1";  
+        let rekognition = new AWS.Rekognition();
+        let params = {
+          Image: {
+          Bytes: e.target.result
+        },
+        Attributes: [
+        'ALL',
+      ]
+    };
+    rekognition.detectFaces(params, function (err, data) {
         console.log(params);
         console.log(data);
-      let table = "<table><tr><th>Low</th><th>High</th></tr>";
-      // show each face and build out estimated age table
-      for (let i = 0; i < data.FaceDetails.length; i++) {
-        table +=
-          "<tr><td>" +
-          data.FaceDetails[i].AgeRange.Low +
-          "</td><td>" +
-          data.FaceDetails[i].AgeRange.High +
-          "</td></tr>";
-      }
-      table += "</table>";
-      document.getElementById("faceResult").innerHTML = table;
-    }
-  });
-}
+      //if (err) console.log(err, err.stack); // an error occurred
+      //else {
+    //    var table = "<table><tr><th>Low</th><th>High</th></tr>";
+    //     // show each face and build out estimated age table
+    //     for (var i = 0; i < data.FaceDetails.length; i++) {
+    //       table += '<tr><td>' + data.FaceDetails[i].AgeRange.Low +
+    //         '</td><td>' + data.FaceDetails[i].AgeRange.High + '</td></tr>';
+    //     }
+    //     table += "</table>";
+    //     document.getElementById("faceResult").innerHTML = table;
+     // }
+    });
 
-export default function ProcessImage() {
-  console.log("Did called");
-  let control = document.getElementById("fileToUpload");
-  let file = control.files[0];
-
-  // Load Base64 image encoder
-  let reader = new FileReader();
-  reader.onload = (function (theFile) {
-    return function (event) {
-      let img = document.createElement("img");
-      let image = null;
-      img.src = event.target.result;
-      let jpg = true;
-      try {
-        image = atob(event.target.result.split("data:image/jpg;base64,")[1]);
-      } catch (error) {
-        jpg = false;
-      }
-      if (jpg == false) {
-        try {
-          image = atob(event.target.result.split("data:image/png;base64,")[1]);
-        } catch (error) {
-          alert("Not the image AWS Rekognition can process. Please try again");
-          return;
-        }
-      }
-
-      // Unencode Image Bytes for Rekognition API
-      let length = image.length;
-      let imageBytes = new ArrayBuffer(length);
-      let ua = new Uint8Array(imageBytes);
-      for (var i = 0; i < length; i++) {
-        ua[i] = image.charCodeAt(i);
-      }
-
-      // Call Rekognition
-      DetectFaces(imageBytes);
-    };
-  })(file);
-
-  reader.readAsDataURL(file);
-}
+      };
+    })(file);
+    reader.readAsArrayBuffer(file);
+  }
+function AnonLog() {
+    
+    // Configure the credentials provider to use your identity pool
+    AWS.config.region = 'RegionToUse'; // Region
+    AWS.config.credentials = new AWS.Credentials({
+      IdentityPoolId: 'IdentityPoolIdToUse',
+    });
+    // Make the call to obtain credentials
+    AWS.config.credentials.get(function () {
+      // Credentials will be available when this function is called.
+      var accessKeyId = AWS.config.credentials.accessKeyId;
+      var secretAccessKey = AWS.config.credentials.secretAccessKey;
+      var sessionToken = AWS.config.credentials.sessionToken;
+    });
+  }
 
 
