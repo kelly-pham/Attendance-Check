725a0aa394b54d5d53ce6852796b309f1324a9e1
Kelly Pham <kelly.pham012@gmail.com>
2020-11-07 00:23:46 -0800

    Updated and fixed error for images link

--- src/components/ProcessImage.js
+++ src/components/ProcessImage.js
@@ -1,21 +1,23 @@
+import {Storage} from "@aws-amplify/storage";
+import { useState } from "react";
 require("dotenv");
 
 
+
 let AWS = require("aws-sdk");
-var BucketName = 'image-store115601-dev';
-var s3 = new AWS.S3({
-  params:{Bucket: BucketName},
-        //   credentials:{
-        //   accessKeyId: process.env.AWS_AccessKey,
-        //   secretAccessKey: process.env.AWS_SecretAccessKey
-        // },
-});
+// var BucketName = 'image-store115601-dev';
+// var s3 = new AWS.S3({
+//   params:{Bucket: BucketName},
+//         //   credentials:{
+//         //   accessKeyId: process.env.AWS_AccessKey,
+//         //   secretAccessKey: process.env.AWS_SecretAccessKey
+//         // },
+// });
 export default function ProcessImage(collectionId) {
   AnonLog();
   var control = document.getElementById("fileToUpload");
   var file = control.files[0];
-  
-
+ 
   // Load base64 encoded image for display
   let reader = new FileReader();
   reader.onload = (function (theFile) {
@@ -34,11 +36,11 @@ export default function ProcessImage(collectionId) {
 
       let params = {
         CollectionId: collectionId,
-        FaceMatchThreshold: 85,
+        FaceMatchThreshold: 80,
         Image: {
           Bytes: e.target.result,
         },
-        MaxFaces: 1,
+        MaxFaces: 200,
       };
       rekognition.searchFacesByImage(params, function (err, data) {
         if (err) {
@@ -50,41 +52,40 @@ export default function ProcessImage(collectionId) {
         if (err) {
           console.log(err);
         } else {
-            // Define S3 bucket 
-          var s3Bucket = 'https://image-store115601-dev.s3.amazonaws.com/public/';
-        
-       
-          let table = `<h1>Faces found in ${collectionId}</h1><table id = 'faces-table'><tr><th style = "text-align:center">ID</th><th style = "text-align:center">Names</th><th style = "text-align:center">Image Link</th></tr>`;
+          let name1 = '';
           let face_array = data.FaceMatches.length;
+          console.log(face_array);
           if (face_array !== 0) {
-            for (var i = 0; i < face_array; i++) {
-              let id = i+1;
-              
-              let externalImageID =
-                data.FaceMatches[i]["Face"]["ExternalImageId"];
+            for (var i = 0; i < face_array; i++) {         
+              let externalImageID =data.FaceMatches[i]["Face"]["ExternalImageId"];
               let name = externalImageID.split("-")[2];
-              console.log(name);
-              
-              var params = {
-                Bucket: BucketName,
-                Key: `public/${collectionId}/${name}`,
-        //         credentials:{
-        //   accessKeyId: process.env.AWS_AccessKey,
-        //   secretAccessKey: process.env.AWS_SecretAccessKey
-        // },
-        Expires: 5 * 60
-              };
-              // var generate_url = s3.getSignedUrl('getObject',params);
-              // console.log(generate_url);
-              //var url = `<a href = '${generate_url}'>View Face</a>`;
-              var imageLink = `<a href = '${s3Bucket}${collectionId}/${name}>View Face</a>`;
-              table += "<tr><td>" + id + "</td><td>" + name.split(".")[0] + "</td><td>" + imageLink + "</td></tr>";
+              name1 = name;
+              console.log(name1);
+              var test_link = `${collectionId}/${name1}`;
+          console.log(test_link);
+          Storage.get(test_link).then((result) => {console.log(result);return result;}).catch((err) => console.log(err)).then(function(result){
+            let table = `<h1>Faces found in ${collectionId}</h1><table id = 'faces-table'><tr><th style = "text-align:center">ID</th><th style = "text-align:center">Names</th><th style = "text-align:center">Image Link</th></tr>`;
+            let face_array = data.FaceMatches.length;
+            if (face_array !== 0) {
+              for (var i = 0; i < face_array; i++) {
+                let id = i+1;
+                
+                let externalImageID =data.FaceMatches[i]["Face"]["ExternalImageId"];
+                let name = externalImageID.split("-")[2];
+                console.log(name);
+                var imageLink = `<a href = ${JSON.stringify(result)}>View Face</a>`;
+                console.log(imageLink);
+                table += "<tr><td>" + id + "</td><td>" + name.split(".")[0] + "</td><td>" + imageLink + "</td></tr>";
+                
+              }
+              table += "</table>";
+              document.getElementById("faceResult").innerHTML = table;
             }
-            table += "</table>";
-            document.getElementById("faceResult").innerHTML = table;
-
-            // let test = data.FaceMatches.Face.ExternalImageId;
-          }
+          });
+            }}
+          
+          
+         
         }
 
       
@@ -121,3 +122,4 @@ function AnonLog() {
     var sessionToken = AWS.config.credentials.sessionToken;
   });
 }
+
